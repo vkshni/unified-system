becc80de609907c7f960aed63294c5db1e68673b
vkshni <vksdrive24@gmail.com>
2026-03-30 21:33:25 +0530

    Systems info

---
+++ config/systems.py
@@ -0,0 +1,39 @@
+# Systems
+SYSTEMS = {
+    "idgen": {
+        "name": "ID Generator",
+        "description": "Generate IDs with prefixes",
+        "module": "modules.idgen.main",
+        "protected": False,
+    },
+    "penny": {
+        "name": "Penny Tracker",
+        "description": "Track expenses",
+        "module": "modules.penny.main",
+        "protected": True,
+    },
+    "taski": {
+        "name": "Task Manager",
+        "description": "Manage tasks",
+        "module": "modules.taski.main",
+        "protected": False,
+    },
+    "shield": {
+        "name": "Password Manager",
+        "description": "Manage passwords",
+        "module": "modules.shield.main",
+        "protected": True,
+    },
+    "shorturl": {
+        "name": "URL Shortener",
+        "description": "Shortens URL",
+        "module": "modules.shorturl.main",
+        "protected": False,
+    },
+    "snippet": {
+        "name": "Snippet Manager",
+        "description": "Manage snippet",
+        "module": "modules.snippet.main",
+        "protected": True,
+    },
+}
